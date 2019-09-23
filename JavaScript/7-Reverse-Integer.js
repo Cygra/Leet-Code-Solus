8c2f08f3acb36cc100ea104f44099fdf01f9bc0a
Cygra <sjtuwbh@gmail.com>
2019-09-23 21:46:31 +0800

    rename files

---
+++ JavaScript/7-Reverse-Integer.js
@@ -0,0 +1,33 @@
+// Given a 32-bit signed integer, reverse digits of an integer.
+
+// Example 1:
+// >Input: 123
+// >Output: 321
+
+// Example 2:
+// >Input: -123
+// >Output: -321
+
+// Example 3:
+// >Input: 120
+// >Output: 21
+
+// Note:
+
+// Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2<sup>31</sup>,  2<sup>31</sup> − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  var r = parseInt(
+    Math.abs(x)
+      .toString()
+      .split("")
+      .reverse()
+      .join("")
+  );
+  const reversed = x.toString().split("")[0] === "-" ? -1 * r : r;
+  return reversed <= 0x7fffffff && reversed >= -0x80000000 ? reversed : 0;
+};
---
+++ JavaScript/9-Palindrome-Number.js
@@ -0,0 +1,39 @@
+// Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+// Example 1:
+// > Input: 121
+// > Output: true
+
+// Example 2:
+// > Input: -121
+// > Output: false
+// > Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+// Example 3:
+// > Input: 10
+// > Output: false
+// > Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+// Follow up:
+
+// Coud you solve it without converting the integer to a string?
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  var arr = x.toString().split("");
+  var len = arr.length;
+  if (arr[0] === "-") return false;
+  var result = [];
+  var l = len % 2 === 0 ? len / 2 : (len + 1) / 2;
+  for (var i = 0; i < l; i++) {
+    if (arr[i] === arr[len - 1 - i]) {
+      result.push(0);
+    } else {
+      result.push(1);
+    }
+  }
+  return !result.includes(1);
+};
