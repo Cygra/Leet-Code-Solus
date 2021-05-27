fe711357b1fd2d31a47dc16f4b8cb5df23a01310
Cygra <sjtuwbh@gmail.com>
2021-05-27 19:42:50 +0800

    update

--- Main.java
+++ Main.java
@@ -1,21 +1,31 @@
-class Solution {
-  public int[] countBits(int n) {
-    int[] bits = new int[n + 1];
-    bits[0] = 0;
-    for (int i = 1; i <= n; i++) {
-      if ((i & 1) == 0) {
-        // 偶数
-        bits[i] = bits[i >> 1];
-      } else {
-        // 奇数
-        bits[i] = bits[i - 1] + 1;
-      }
-    }
-    return bits;
-  }
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
-  public static void main(String[] args) {
-    Solution s = new Solution();
-    System.out.print(s.countBits(5));
-  }
+import javax.swing.tree.TreeNode;
+
+import java.util.ArrayList;
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        LinkedList<TreeNode> s = new LinkedList<>();
+        List<Integer> r = new ArrayList<Integer>();
+        TreeNode c = root;
+        while (c != null || !s.isEmpty()) {
+            if (c != null) {
+                s.push(c);
+                c = c.left;
+            } else {
+                TreeNode n = s.pop();
+                r.add(n.val);
+                c = c.right;
+            }
+        }
+        return r;
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(new Solution().longestConsecutive( new int[] {100,4,200,1,3,2}));
+    }
 }
